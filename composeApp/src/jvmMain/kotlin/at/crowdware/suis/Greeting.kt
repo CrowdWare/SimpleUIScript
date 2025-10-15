954403c25d9d71c94c576bdf936c58fcc85def74
Artanidos <japp.olaf@gmail.com>
2025-10-15 07:07:13 +0200

    upd

--- composeApp/src/jvmMain/kotlin/at/crowdware/suis/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package at.crowdware.suis
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/jvmMain/kotlin/at/crowdware/suis/Platform.kt
+++
@@ -1,7 +0,0 @@
-package at.crowdware.suis
-
-class JVMPlatform {
-    val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-fun getPlatform() = JVMPlatform()
